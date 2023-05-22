5dbd2994016b75afffe59a13c0cfdc2bde381ee6
unknown <marquez.cmbh@gmail.com>
2023-05-22 13:11:54 -0300

    build: initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+// app.use(router);
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
